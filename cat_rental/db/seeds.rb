a55d1b370b002faa5080b671acf81917f974b111
sean <midruss@yahoo.com>
2022-01-13 17:17:08 -0500

    Completed index view and started show view

--- cat_rental/db/seeds.rb
+++ cat_rental/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cat1 = Cat.create(birth_date: '2005/01/20', color: 'brown', name: 'Coco', sex: 'F', description: 'playing with toys')
+cat1 = Cat.create(birth_date: '2000/03/15', color: 'black', name: 'Ebony', sex: 'M', description: 'very mean!')
+cat1 = Cat.create(birth_date: '2020/04/29', color: 'orange', name: 'Tiger', sex: 'F', description: 'jumps a lot')
+
