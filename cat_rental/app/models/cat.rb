e21a679cffa45777334f8ed31a1b69b2885f3e43
Ethan Lam <ethanlam0@yahoo.com>
2022-01-13 15:43:39 -0500

    Finish building Cat model

--- cat_rental/app/models/cat.rb
+++ cat_rental/app/models/cat.rb
@@ -1,4 +1,8 @@
+require 'action_view'
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
     COLORS = [
         "orange",
         "black",
@@ -7,17 +11,18 @@ class Cat < ApplicationRecord
     ]
 
 
-    validates :date, :name, presence: true
+    validates :birth_date, :name, presence: true
     validates :color, inclusion: { in: COLORS,
         message: "Not a valid color" }
+                        #inclusion: [COLORS]
     validates :sex, inclusion: { in: ["M", "F"],
         message: "Must enter M or F" }, presence: true #do you need inclusion AND presence?
         
+    
 
     def age
-
+        time_ago_in_words(birth_date)
     end
-
 
 
 end
