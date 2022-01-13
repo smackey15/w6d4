2015ba04e3281cd4800929d3516d157e7f3320b6
Ethan Lam <ethanlam0@yahoo.com>
2022-01-13 14:38:03 -0500

    Finish sex and color validations

--- cat_rental/app/models/cat.rb
+++ cat_rental/app/models/cat.rb
@@ -1,3 +1,24 @@
 class Cat < ApplicationRecord
-    validates    
+    COLORS = [
+        "orange",
+        "black",
+        "brown",
+        "grey"
+    ]
+
+
+    validates :date, :name, presence: true
+    validates :color, inclusion: { in: COLORS,
+        message: "Not a valid color" }
+    validates :sex, inclusion: { in: ["M", "F"],
+        message: "Must enter M or F" }
+
+        
+
+    def age
+
+    end
+
+
+
 end
